9c0146f75dac171264f2469447b6542a3556f07b
SebasNatale <giordano_95@hotmail.com>
2021-03-28 14:10:20 -0500

    Back home button done, initial interface tests

--- src/components/musicRoom.tsx
+++ src/components/musicRoom.tsx
@@ -1,16 +1,17 @@
 import './styles/MusicRoom.css';
 
 function MusicRoom() {
-    return <>
+    return (<div className="musicRoom">
         <div className="warmUpVideoFrame">
-            <h1>First things first</h1>
+            <h1>✨ First things first ✨</h1>
             <iframe className="warmUpVideo" src="https://www.youtube-nocookie.com/embed/e28xk4YnUAA?start=142" allow="encrypted-media;"></iframe>
             <i className="fas fa-chevron-down"></i>
         </div>
-        <div id="warmUpInterface">
-            <button className="backHomeButton" type="button" onClick={() => window.location.reload()}>Go back</button>
+        <button className="backHomeButton" type="button" onClick={() => window.location.reload()}>Go back</button>
+        <div id="navBar">
+            <h1>TEST_TEST_TEST_TEST_TEST</h1>
         </div>
-    </>
+    </div>)
 };
 
 export default MusicRoom;
