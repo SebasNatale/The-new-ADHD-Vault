5a277f7735cd4e037888b9d6cadacba6a947413b
SebasNatale <giordano_95@hotmail.com>
2021-03-28 11:28:56 -0500

    Positioning bug fixed, favicon added, title changed

--- src/components/musicRoom.tsx
+++ src/components/musicRoom.tsx
@@ -2,16 +2,10 @@ import './styles/MusicRoom.css';
 
 function MusicRoom() {
     return <>
-        <div id="musicRoom">
-
-            <div className="ESTE DIV NO SIRVE DE NI MIERDA">
-                <div className="warmUpVideoFade">
-                    <h1>First things first</h1>
-                    <iframe className="warmUpVideo" src="https://www.youtube-nocookie.com/embed/e28xk4YnUAA?start=142" allow="encrypted-media;"></iframe>
-                    <i className="fas fa-chevron-down"></i>
-                </div>
-            </div>
-
+        <div className="warmUpVideoFrame">
+            <h1>First things first</h1>
+            <iframe className="warmUpVideo" src="https://www.youtube-nocookie.com/embed/e28xk4YnUAA?start=142" allow="encrypted-media;"></iframe>
+            <i className="fas fa-chevron-down"></i>
         </div>
         <div id="warmUpInterface">
             <button className="backHomeButton" type="button" onClick={() => window.location.reload()}>Go back</button>
